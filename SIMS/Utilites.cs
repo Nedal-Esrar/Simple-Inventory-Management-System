fc5f5535a3505b2554343043e991c6e009e86e46
Nedal-Esrar <113736086+Nedal-Esrar@users.noreply.github.com>
2023-07-22 23:49:24 +0300

    Add Capitalize utility extension method
    This extension method returns a capitalized version of the invoking string.

--- SIMS/Utilites.cs
+++ SIMS/Utilites.cs
@@ -2,5 +2,15 @@ namespace SIMS;
 
 public static class Utilites
 {
+  public static string Capitalize(this string str)
+  {
+    if (string.IsNullOrWhiteSpace(str))
+    {
+      return str;
+    }
+
+    return char.ToUpper(str[0]) + str.Substring(1).ToLower();
+  }
+  
   
 }
