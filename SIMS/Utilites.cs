d8d6f061ab58aa78d470a63f028a43662abd25ec
Nedal-Esrar <113736086+Nedal-Esrar@users.noreply.github.com>
2023-07-22 23:50:53 +0300

    Add GetStringInput utility method
    This method is used to prompt the user to enter a valid string according
    to the provided prompt until a valid one is entered.

--- SIMS/Utilites.cs
+++ SIMS/Utilites.cs
@@ -12,5 +12,22 @@ public static class Utilites
     return char.ToUpper(str[0]) + str.Substring(1).ToLower();
   }
   
-  
+  /// <summary>
+  /// Prompts the user to input a non-empty string and loops until a valid string input is provided.
+  /// A valid string is a string that contains at least one non-whitespace character.
+  /// </summary>
+  /// <param name="prompt">The prompt displayed to the user</param>
+  /// <returns>The valid string input</returns>
+  public static string GetStringInput(string prompt)
+  {
+    string name;
+
+    do
+    {
+      Console.Write($"{prompt}: ");
+      name = Console.ReadLine();
+    } while (string.IsNullOrWhiteSpace(name));
+
+    return name.Trim();
+  }
 }
