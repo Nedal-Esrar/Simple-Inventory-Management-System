748b776f9c37d4ab37f3f3679306d6e40abbb34d
Nedal-Esrar <113736086+Nedal-Esrar@users.noreply.github.com>
2023-07-23 10:31:08 +0300

    Add `Run` method in `InventoryManagementSystem` class
    Implement `Run` method as the entry point of the `InventoryManagementSystem` instance.

--- SIMS/InventoryManagementSystem.cs
+++ SIMS/InventoryManagementSystem.cs
@@ -1,3 +1,5 @@
+using Utils = SIMS.Utilites;
+
 namespace SIMS;
 
 public class InventoryManagementSystem
@@ -9,5 +11,36 @@ public class InventoryManagementSystem
     _productInventory = new();
   }
   
-  
+  public void Run()
+  {
+    Console.WriteLine("Welcome to Simple Inventory Management System!");
+    
+    Utils.DisplayStars();
+
+    Console.WriteLine("Note: For any invalid input, you will be prompted again to enter a correct value.");
+
+    bool exit = false;
+
+    while (!exit)
+    {
+      Utils.DisplayStars();
+      
+      Console.WriteLine("Please pick a choice [1..6]");
+
+      Utils.DisplayChoicesMenu();
+      
+      Utils.DisplayStars();
+
+      string choice = Utils.GetStringInput("Choice");
+      
+      Utils.DisplayStars();
+
+      exit = HandleChoice(choice);
+    }
+  }
+
+  private bool HandleChoice(string choice)
+  {
+    throw new NotImplementedException();
+  }
 }
