eab03db36ce2a320472304ad3a5e3ddad75dfcce
Nedal-Esrar <113736086+Nedal-Esrar@users.noreply.github.com>
2023-07-23 10:48:55 +0300

    Add `HandleChoice` method
    Implemented the `HandleChoice` method in the `InventoryManagementSystem` class. This method allows the application to process user menu choices and perform the associated actions based on the selected option.

--- SIMS/InventoryManagementSystem.cs
+++ SIMS/InventoryManagementSystem.cs
@@ -39,7 +39,73 @@ public class InventoryManagementSystem
     }
   }
 
+  /// <summary>
+  /// Chooses the appropriate handler according to the choice.
+  /// returns a boolean that indicates whether to terminate the application or not.
+  /// </summary>
+  /// <param name="choice"></param>
+  /// <returns>
+  /// True if the choice was exit choice (6);
+  /// False otherwise.
+  /// </returns>
   private bool HandleChoice(string choice)
+  {
+    switch (choice)
+    {
+      case "1":
+        HandleAddChoice();
+        
+        break;
+      case "2":
+        HandleViewAllChoice();
+        
+        break;
+      case "3":
+        HandleEditChoice();
+        
+        break;
+      case "4":
+        HandleDeleteChoice();
+        
+        break;
+      case "5":
+        HandleSearchChoice();
+        
+        break;
+      case "6":
+        Console.WriteLine("Thank you for using Simple Inventory Management System ;)");
+
+        return true;
+      default:
+        Console.WriteLine("the choice is not in the range [1..6]");
+        
+        break;
+    }
+
+    return false;
+  }
+
+  private void HandleAddChoice()
+  {
+    throw new NotImplementedException();
+  }
+
+  private void HandleViewAllChoice()
+  {
+    throw new NotImplementedException();
+  }
+
+  private void HandleEditChoice()
+  {
+    throw new NotImplementedException();
+  }
+
+  private void HandleDeleteChoice()
+  {
+    throw new NotImplementedException();
+  }
+
+  private void HandleSearchChoice()
   {
     throw new NotImplementedException();
   }
