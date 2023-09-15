e7780aa4f447c8c8dc49084474e48356f9bae95b
Nedal-Esrar <113736086+Nedal-Esrar@users.noreply.github.com>
2023-09-15 21:55:08 +0300

    Move Product Model to the Models directory and Make necessary Changes

---
+++ SIMS/Models/Product.cs
@@ -0,0 +1,12 @@
+namespace SIMS.Models;
+
+public class Product : IReadOnlyProduct
+{
+  public int Id { get; set; }
+  
+  public string Name { get; set; }
+  
+  public decimal Price { get; set; }
+  
+  public int Quantity { get; set; }
+}
