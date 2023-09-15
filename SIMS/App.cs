f3422c75762a83e327f00ad08299461cd32fb6aa
Nedal-Esrar <113736086+Nedal-Esrar@users.noreply.github.com>
2023-09-15 22:14:30 +0300

    Make use of DI containers in the app entry point

--- SIMS/App.cs
+++ SIMS/App.cs
@@ -1,5 +1,17 @@
+using AutoMapper;
+using Microsoft.Extensions.DependencyInjection;
 using SIMS;
+using SIMS.Inventories;
+using SIMS.Repositories;
 
-InventoryManagementSystem app = new();
+var serviceProvider = new ServiceCollection()
+  .AddScoped<IProductRepository, InMemoryProductRepository>()
+  .AddScoped<IMapper>(_ => new MapperConfiguration(_ => { }).CreateMapper())
+  .AddScoped<IInventory, Inventory>()
+  .AddScoped<InventoryManagementSystem>();
 
-app.Run();
+var app = serviceProvider
+  .BuildServiceProvider()
+  .GetRequiredService<InventoryManagementSystem>();
+
+await app.Run();
