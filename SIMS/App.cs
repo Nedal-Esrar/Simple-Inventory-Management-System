c48c0f6f7512e2b5d31d93cebe35964c88f2246d
Nedal-Esrar <113736086+Nedal-Esrar@users.noreply.github.com>
2023-09-16 12:56:37 +0300

    Fix mapper error throwing exception every time Map is invoked
    The error was present because mapper settings was not set. Settings was set in this commit.

--- SIMS/App.cs
+++ SIMS/App.cs
@@ -1,12 +1,18 @@
 using AutoMapper;
 using Microsoft.Extensions.DependencyInjection;
 using SIMS;
+using SIMS.Dtos;
 using SIMS.Inventories;
+using SIMS.Models;
 using SIMS.Repositories;
 
 var serviceProvider = new ServiceCollection()
   .AddScoped<IProductRepository, InMemoryProductRepository>()
-  .AddScoped<IMapper>(_ => new MapperConfiguration(_ => { }).CreateMapper())
+  .AddScoped<IMapper>(_ => new MapperConfiguration(cfg =>
+  {
+    cfg.CreateMap<Product, ProductDto>();
+    cfg.CreateMap<ProductDto, Product>();
+  }).CreateMapper())
   .AddScoped<IInventory, Inventory>()
   .AddScoped<InventoryManagementSystem>();
 
