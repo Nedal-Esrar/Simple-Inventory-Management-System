8d8923f49b1b024621b838ed8fc12c7749e806b8
Nedal-Esrar <113736086+Nedal-Esrar@users.noreply.github.com>
2023-09-16 14:25:39 +0300

    Add `SqlProductRepository`

---
+++ SIMS/Repositories/SqlRepo/SqlProductRepository.cs
@@ -0,0 +1,52 @@
+using SIMS.Models;
+
+namespace SIMS.Repositories.SqlRepo;
+
+public class SqlProductRepository : IProductRepository
+{
+  private readonly ISqlConnectionFactory _connectionFactory;
+
+  private readonly IDapperWrapper _dapperWrapper;
+
+  public SqlProductRepository(ISqlConnectionFactory connectionFactory, IDapperWrapper dapperWrapper)
+  {
+    _connectionFactory = connectionFactory;
+
+    _dapperWrapper = dapperWrapper;
+  }
+
+  public async Task<IEnumerable<Product>> GetAll()
+  {
+    using var dbConnection = _connectionFactory.CreateConnection();
+
+    return await _dapperWrapper.GetAll(dbConnection, SqlCommands.GetAllProducts);
+  }
+
+  public async Task<Product?> GetByName(string name)
+  {
+    using var dbConnection = _connectionFactory.CreateConnection();
+      
+    return await _dapperWrapper.GetByName(dbConnection, SqlCommands.GetProductByName, new { name });
+  }
+
+  public async Task Add(Product product)
+  {
+    using var dbConnection = _connectionFactory.CreateConnection();
+
+    await _dapperWrapper.Add(dbConnection, SqlCommands.AddProduct, product);
+  }
+
+  public async Task Update(Product product)
+  {
+    using var dbConnection = _connectionFactory.CreateConnection();
+
+    await _dapperWrapper.Update(dbConnection, SqlCommands.UpdateProduct, product);
+  }
+
+  public async Task Delete(Product product)
+  {
+    using var dbConnection = _connectionFactory.CreateConnection();
+
+    await _dapperWrapper.Delete(dbConnection, SqlCommands.DeleteProduct, product);
+  }
+}
