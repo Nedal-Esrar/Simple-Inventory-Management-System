04adf76fdb12a76f2624a224ffc7c723d8cf0184
Nedal-Esrar <113736086+Nedal-Esrar@users.noreply.github.com>
2023-09-16 14:30:16 +0300

    Modify app entry point to use sql repo

--- SIMS/App.cs
+++ SIMS/App.cs
@@ -1,13 +1,21 @@
 using AutoMapper;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using SIMS;
 using SIMS.Dtos;
 using SIMS.Inventories;
 using SIMS.Models;
 using SIMS.Repositories;
+using SIMS.Repositories.SqlRepo;
+
+var configuration = new ConfigurationBuilder()
+  .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+  .Build();
 
 var serviceProvider = new ServiceCollection()
-  .AddScoped<IProductRepository, InMemoryProductRepository>()
+  .AddScoped<ISqlConnectionFactory>(_ => new SqlConnectionFactory(configuration.GetConnectionString("SqlConnection")))
+  .AddScoped<IDapperWrapper, DapperWrapper>()
+  .AddScoped<IProductRepository, SqlProductRepository>()
   .AddScoped<IMapper>(_ => new MapperConfiguration(cfg =>
   {
     cfg.CreateMap<Product, ProductDto>();
